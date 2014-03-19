aea42cf65e6a4372349e8cec3f43c15eeb4e0856
unknown <qiyuuu@gmail.com>
2014-03-19 17:54:55 +0800

    change start tiles to 2 and *2

--- js/game_manager.js
+++ js/game_manager.js
@@ -51,8 +51,12 @@ GameManager.prototype.setup = function () {
 
 // Set up the initial tiles to start the game with
 GameManager.prototype.addStartTiles = function () {
+  var tile, types = ['number', 'operator'];
   for (var i = 0; i < this.startTiles; i++) {
-    this.addRandomTile();
+    if (this.grid.cellsAvailable()) {
+      tile = new Tile(this.grid.randomAvailableCell(), 2, types[i % types.length]);
+      this.grid.insertTile(tile);
+    }
   }
 };
 

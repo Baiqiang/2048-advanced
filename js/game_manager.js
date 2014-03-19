3b3fbbf9ff68ef346788c217545de0e4ad713607
unknown <qiyuuu@gmail.com>
2014-03-19 16:06:57 +0800

    fix bugs

--- js/game_manager.js
+++ js/game_manager.js
@@ -239,7 +239,7 @@ GameManager.prototype.tileMatchesAvailable = function () {
 
           var other  = self.grid.cellContent(cell);
 
-          if (other && (tile.type === 'operator' || tile.type === 'operator' || other.value === tile.value)) {
+          if (other && (other.type === 'operator' || tile.type === 'operator' || other.value === tile.value)) {
             return true; // These two tiles can be merged
           }
         }

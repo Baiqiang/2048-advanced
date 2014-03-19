a5d0e938f757cc64a529d4991001028d971f46eb
unknown <qiyuuu@gmail.com>
2014-03-19 15:55:03 +0800

    update

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -29,7 +29,8 @@ HTMLActuator.prototype.actuate = function (grid, metadata) {
   });
 };
 
-HTMLActuator.prototype.restart = function () {
+// Continues the game (both restart and keep playing)
+HTMLActuator.prototype.continue = function () {
   this.clearMessage();
 };
 
